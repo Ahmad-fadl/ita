1d603983a8db1c3a7273e8ecdd99d1c1b88c5ffd
SinaLaura <sinadenzel@gmail.com>
2021-02-08 21:32:09 +0100

    Implement Long Tweets Hydration method so the tweets won't be cut off

--- src/Twitter-Access.py
+++ src/Twitter-Access.py
@@ -4,7 +4,7 @@
 import time
 from tqdm import tqdm
 from twython import Twython
-import json
+import twython.exceptions
 import pandas as pd
 import os
 from pprint import pprint
@@ -32,20 +32,6 @@ import json
 # credentials['BEARER_TOKEN'] = 'xxx' # seee celll above
 # comment out these lines after you have saved them to twitter_credentials.json to ita_project/
 
-log_into_file = True
-
-
-def print_log(msg):
-    """Use instead of print, to print and log.
-    Set global variable 'log_into_file to False, is logging not wished."""
-    global log_into_file
-    tqdm.write(msg)  # similar to print, but better optics when using tqdm loading bar
-    if log_into_file:
-        with open("data/Hydrated_Tweets_Report.txt", "a") as log:
-            log.write(msg + "\n")
-
-
-start = time.time()
 
 # Load credentials from json file
 with open("twitter_credentials.json", "r") as file:
@@ -57,7 +43,6 @@ python_tweets = Twython(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRE
 
 
 # show json for a specific ID using show_status()
-
 # tweet= python_tweets.show_status(id="1245219745311404032")
 # pprint(tweet)
 
@@ -68,12 +53,24 @@ python_tweets = Twython(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRE
 # print(tweet["created_at"].split()[1])
 # print(tweet["created_at"].split()[2])
 
-
-# request limit: 900 requestis/15 mins.
+# request limit: 900 requests/15 mins.
 # so if you make 1 request per second, everything will be fine. (15*60 secs = 900secs)
-# but it seems to be a rolling window, so you don't have to wait for a full reset, when limit is reached. 
-# adjust sleeping time as required. 
+# Rolling window? -> No
 # DONE dynamische Berechnung der time-limits um ensprechend anfragefrequenz automatisch regeln
+
+
+
+log_into_file = True
+
+
+def print_log(msg):
+    """Use instead of print, to print and log.
+    Set global variable 'log_into_file to False, is logging not wished."""
+    global log_into_file
+    tqdm.write(msg)  # similar to print, but better optics when using tqdm loading bar
+    if log_into_file:
+        with open("data/Hydrated_Tweets_Report_Long.txt", "a") as log:
+            log.write(msg + "\n")
 
 
 def calc_delay_to_not_run_into_rate_limit(delay=True, request_method='show') -> float:
@@ -90,13 +87,9 @@ def calc_delay_to_not_run_into_rate_limit(delay=True, request_method='show') -> 
     Returns:
         seconds to sleep before next request
     """
-    if not delay:
-        return 0
-
     resource = python_tweets.get_application_rate_limit_status(resources='statuses')
     # if you get a permission denied error here, you have to change your "project settings" here (developer.twitter.com)
     # to "Read, Write, and Direct Messages" and regenerate all keys.
-    # TODO Does this error occur or am I wrong? (I got this error, but maybe for another reason)
 
     if request_method == 'show':
         resource = resource["resources"]["statuses"]["/statuses/show/:id"]
@@ -106,6 +99,11 @@ def calc_delay_to_not_run_into_rate_limit(delay=True, request_method='show') -> 
         # e.g: "{"limit": 900,"remaining": 900, "reset": 1403602426} -- for 100 ID requests at once (same limits)
     else:
         raise NotImplementedError
+
+    print(f" remaining requests until reset (<15mins): {resource['remaining']}")
+
+    if not delay:
+        return 0
 
     if resource["remaining"] == 0:
         return resource["reset"] - time.time()
@@ -117,94 +115,121 @@ def calc_delay_to_not_run_into_rate_limit(delay=True, request_method='show') -> 
     return (resource["reset"] - time.time()) / resource["remaining"]
 
 
+start = time.time()
+
 directory = "data/GeoCOV19TweetsDataset"
 
+if not os.path.exists("data/Hydrated_Tweets_Long"):
+    os.mkdir("data/Hydrated_Tweets_Long")
+
 count_files = 0
+
+# error collecting #
 count_corrupted_files = 0
 tweets_not_found = 0
-count_metaextraction_fail = 0
+count_metadata_fail = 0
+count_other_errors = 0
 
 print_log("--------\n" + time.strftime("%d.%m.%y, %H:%M") + " --- Start hydration and logging...")
+
 # For each csv-file (containing two days)
-for entry in tqdm(os.scandir(directory), total=len(list(os.scandir(directory)))):
-    if not entry.path.endswith(".csv"):
-        print(f"skipped {os.path.basename(entry.path)}")
-    # print (entry.path)
+for csv in tqdm(os.scandir(directory), total=len(list(os.scandir(directory))), desc="Files", position=1):
+    if os.path.exists("Long_" + str(csv)):
+        print(f"skipped {os.path.basename(csv.path)}, because it's already hydrated")
+        continue
+    if not csv.path.endswith(".csv"):
+        print(f"skipped {os.path.basename(csv.path)}")
+
     count_files += 1
-    not_extracted_in_batch = 0
     metadata_fail = 0
 
-    # Load csv file containing the tweet ID's
-    tweets_csv = pd.read_csv(entry.path)  # [:5] #only first few rows for testing
+    # Load csv file containing the tweet IDs
+    tweets_csv = pd.read_csv(csv.path)  # [:20]  # only first few rows for testing
     try:
         tweets_csv.columns = ["ID", "Sentiment"]
     except ValueError as e:
-        print_log(f"{count_files}. File: {entry.path})\n{e} --------------------")
+        print_log(f"{count_files}. File: {csv.path})\n{e} --------------------")
         count_corrupted_files += 1
         continue
+    except Exception as f:
+        count_corrupted_files += 1
+        print(f)
+        continue
 
-    del tweets_csv["Sentiment"]  # or better keep in case we need it later?
+    #del tweets_csv["Sentiment"]  # or better keep, in case we need it later?
 
     # Initialize panda dataframe for the tweets
-    # There will probably be one DF for each day # no, it's 2
     Twitter_Tweets = pd.DataFrame(columns=['ID', 'COUNTRY', 'DAY', 'MONTH', 'TEXT_RAW'])
 
     # Only countries from the following list are considered
     countries_of_interest = ["Vereinigte Staaten", "Republik Indien", "Vereinigtes Königreich"]
 
     ID_list = [tweets_csv['ID'].values[i] for i in range(len(tweets_csv['ID'].values))]
-    # print (ID_list)
+    print(len(ID_list))
 
-    ID_batch_lists = []
-    batch_request = 100  # this is the current limit of the Twitter API for its lookup-batch-method
-    for i in range(0, len(ID_list), batch_request):
-        ID_batch_lists.append(ID_list[i: i + 100])
+    twython_errors = 0
+    other_errors = 0
+    not_of_interest = 0  # count irrelevant countries
 
-    # For each tweets-batch in the file:
+    # For each tweet_ID in the file:
     iteration = -1
-    for batch in ID_batch_lists:  # or use: tqdm(ID_batch_lists):
+    for tweet_ID in tqdm(ID_list, desc=f'Tweets in {os.path.basename(csv.path)}', position=0):
         iteration += 1
-        # Delay the tweet extraction since otherwise the rate limit exceeds 
+        # Delay the tweet extraction since otherwise the rate limit exceeds
         if iteration % 50 == 0:
-            delay = calc_delay_to_not_run_into_rate_limit(delay=True, request_method='lookup')
+            delay = calc_delay_to_not_run_into_rate_limit(delay=True, request_method='show')
+            print("new delay:", ("%.2f" % delay), "secs")
         time.sleep(delay)
 
         try:
-            tweets = python_tweets.lookup_status(id=batch)
-            not_extracted_in_batch += (len(batch) - len(tweets))  # because no error is thrown if ID not found
+            tweet = python_tweets.show_status(id=tweet_ID, tweet_mode='extended')
+        except twython.exceptions.TwythonRateLimitError as too_many_requests:
+            print("This should not have happened. Please call the technician. "
+                  "(And/or restart program - no real harm should have been done.")
+            raise too_many_requests
+        except twython.exceptions.TwythonError as e:
+            twython_errors += 1
+            continue
         except Exception as e:
-            print_log(f"Batch Lookup Error: {e}")
+            other_errors += 1
             continue
 
-        # If all values exist add the tweets ID, Country, Day, Month and Raw Text to the DF
-        for tweet in tweets:
-            try:
-                if str(tweet["place"]["country"]) in countries_of_interest:
-                    data = {
-                        'ID': tweet['id'],
-                        'COUNTRY': tweet["place"]["country"],
-                        'DAY': tweet["created_at"].split()[2],
-                        'MONTH': tweet["created_at"].split()[1],
-                        'TEXT_RAW': tweet["text"]}
-                    Twitter_Tweets = Twitter_Tweets.append(data, ignore_index=True)
-                    # print(tweet["text"])
-            except Exception as meta_fail:
-                metadata_fail += 1
-                print_log(f"Metadata Extraction failed: {meta_fail} (probably no location provided)")
-                continue
+        # If all values exist, add the tweets ID, Country, Day, Month and Raw Text to the DF
+        try:
+            if str(tweet["place"]["country"]) in countries_of_interest:
+                data = {
+                    'ID': tweet['id'],
+                    'COUNTRY': tweet["place"]["country"],
+                    'DAY': tweet["created_at"].split()[2],
+                    'MONTH': tweet["created_at"].split()[1],
+                    'TEXT_RAW': tweet["full_text"]}
+                Twitter_Tweets = Twitter_Tweets.append(data, ignore_index=True)
+                # print(tweet["text"])
+            else:
+                not_of_interest += 1
+
+        except Exception as meta_fail:
+            metadata_fail += 1
+            print(f"Metadata Extraction failed: {meta_fail} (probably no location provided)")
+            continue
 
     # Save DF to csv file
-    Twitter_Tweets.to_csv("data/Hydrated_Tweets/" + os.path.basename(entry.path), index=False, header=True)
+    Twitter_Tweets.to_csv("data/Hydrated_Tweets_Long/Long_" + os.path.basename(csv.path), index=False, header=True)
     print_log("")
-    if not_extracted_in_batch > 0 or metadata_fail > 0:
-        print_log(f"{count_files}. File: {os.path.basename(entry.path)}:\n {not_extracted_in_batch} tweets NOT found."
-                  f"\n {metadata_fail} metadata extractions failed.\n {len(Twitter_Tweets)} tweets extracted")
+    if len(ID_list) != len(Twitter_Tweets):
+        print_log(
+            f"{count_files}. File: {os.path.basename(csv.path)}:\n {twython_errors} tweets NOT found or sth. similar."
+            f"\n {other_errors} other errors."
+            f"\n {metadata_fail} metadata extractions failed.\n {len(Twitter_Tweets)}/{len(ID_list)} tweets extracted."
+            f" ({not_of_interest} tweets not of interest)")
     else:
         print_log(f"NO ERRORS IN FILE {count_files}")
     print_log("--------------------")
 
-    tweets_not_found += not_extracted_in_batch
-    count_metaextraction_fail += metadata_fail
+    # sum up errors
+    count_metadata_fail += metadata_fail
+    tweets_not_found += twython_errors
+    count_other_errors += other_errors
 
 duration = time.time() - start
 duration = f"{duration // 3600}h {duration // 60 % 60}m {int(duration % 60)}s"
@@ -213,6 +238,6 @@ print_log(f"""-----FINAL REPORT------
         {time.strftime("%d.%m.%y, %H:%M")}
         Looked through {count_files} files.
         {tweets_not_found} tweets not found.
-        Metadata extraction failed {count_metaextraction_fail} times
+        Metadata extraction failed {count_metadata_fail} times
         {count_corrupted_files} corrupted .csv-files
         execution duration: {duration}\n\n\n""")
---
+++ src/data/Hydrated_Tweets_Long/readme.md
@@ -0,0 +1,3 @@
+This is where the long tweets go to. They replace the former dir 'Hydrated_Tweets', the tweets there were cut off.   
+Twitter-Access was updated, so now the long tweets will be hydrated (but that will have a longer duration) using another extraction method.
+For further use - after hydrating - you should rename this folder back to Hydrated_Tweets, so the further processing uses these tweets, not the short ones.
